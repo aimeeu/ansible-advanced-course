58ca14c80bf2c46f451cca3a1c5fbd1be632704e
Aimee Ukasick <aimee.ukasick@hashicorp.com>
2026-05-15 16:39:01 -0500

    Removed files that should not be committed. Add .gitignore

--- shared/terraform/multipass-compute/output.tf
+++ shared/terraform/multipass-compute/output.tf
@@ -5,7 +5,3 @@ output "instance_ips" {
 output "instance_names" {
   value = [for instance in data.multipass_instance.instance : instance.name]
 }
-
-output "ssh_key" {
-  value = var.instance_ssh_key
-}
